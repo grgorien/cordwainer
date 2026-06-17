e09b5f13b2d9aa2539e140c42930dc6ff8092bd3
William Gregorio <william.gregorio.ny@gmail.com>
2026-06-17 08:13:00 -0400

    fix: intial {} entries most are default but testing it

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import shopify from "vite-plugin-shopify";
 
 export default {
-  themeRoot: "./",
   plugins: [
-    shopify()
+    shopify({
+      themeRoot: "./", // default true but testing purposes
+      sourceCodeDir: "ui",
+      entrypointsDir: "ui/entrypoints" // non exist and ui name not fit well but it's easier to type looks cleaner
+    })
   ]
 }
